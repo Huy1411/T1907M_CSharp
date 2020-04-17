bc16be8d005bd2743cd5fd9a5eb9d3125b378862
Huy1411 <nguyenxuanhuy1411@gmail.com>
2020-04-17 11:18:08 +0700

    lab1

---
+++ T1907M_NXH/Lab1/Fibonacci.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace T1907M_NXH.Lab1
+{
+    public class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Fibonaccii();
+        }
+        static void Fibonacci()
+        {
+            int n;
+            Console.WriteLine("Nhap so n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int f = 0;
+            int f1 = 1, f2 = 1 ;
+            while (f1 + f2 < n)
+            {
+                f = f1 + f2;
+                f2 = f1;
+                f1 = f;
+            }
+            Console.WriteLine("Number = " + n + " => Fibonacci = " +f);
+        }
+    }
+}
